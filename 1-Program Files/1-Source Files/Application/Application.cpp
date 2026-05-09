9f152cebf49e580a444a3f3f1d9240349a57b653
Muhammad <muhammad.moazzam2775@gmail.com>
2026-05-09 11:20:06 -0700

    Initialize application in main function
    Replace empty main function with application initialization and execution.

--- 1-Program Files/1-Source Files/Application/Application.cpp
+++ 1-Program Files/1-Source Files/Application/Application.cpp
@@ -1,14 +1,7 @@
-#include <iostream>
-#include <print>
-
 #include <System Interface/SystemInterface.hpp>
-
-using namespace std;
 using namespace sjcs;
 
 int main() {
-    // Intentionally empty. All wiring is defined in header contracts and implemented elsewhere.
-    print("SJCS Initialized");
-    cin.get();
-    return 0;
+    auto app = Application::create(8000);
+    return app ? app->run() : 1;
 }
